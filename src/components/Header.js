66a0aabd513da1593be8bd1e7bae898f55825da0
Kristopher Lopez <k.lopez8891@live.com>
2022-12-29 00:32:00 -0500

    Bloodmoon Coffee Header

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/Header.css";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <StorefrontIcon className="header__logoImage" fontSize="large" />
+        <h2 className="header__logoTitle">Bloodmoon Coffee</h2>
+      </div>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="nav__item">
+          <span className="nav__itemLingOne">Hello Guest </span>
+          <span className="nav__itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav__item">
+          <span className="nav__itemLingOne">Your </span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+        <div className="nav__item">
+          <ShoppingCartIcon
+            className="nav__itemShoppingCart"
+            fontSize="large"
+          />
+          <span className="nav__itemLineTwo nav__shoppingCartCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
