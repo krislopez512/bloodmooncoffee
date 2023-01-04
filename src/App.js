bc11af247fb20d92efcab978673b62252c37974c
Kristopher Lopez <k.lopez8891@live.com>
2023-01-04 14:59:30 -0500

    Bloodmoon Coffee Checkout Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
-import Header from './components/Header.js';
-import Home from './components/Home';
+import "./App.css";
+import Header from "./components/Header.js";
+import Home from "./components/Home";
+import Checkout from "./components/Checkout";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,17 @@ import "../styles/Header.css";
 import StorefrontIcon from "@mui/icons-material/Storefront";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header__logo">
-        <StorefrontIcon className="header__logoImage" fontSize="large" />
-        <h2 className="header__logoTitle">Bloodmoon Coffee</h2>
-      </div>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <div className="header__logo">
+          <StorefrontIcon className="header__logoImage" fontSize="large" />
+          <h2 className="header__logoTitle">Bloodmoon Coffee</h2>
+        </div>
+      </Link>
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
@@ -24,10 +27,12 @@ function Header() {
           <span className="nav__itemLingOne">Your </span>
           <span className="nav__itemLineTwo">Shop</span>
         </div>
-        <div className="nav__itemShoppingCart">
-          <ShoppingCartIcon />
-          <span className="nav__itemLineTwo nav__shoppingCartCount">0</span>
-        </div>
+        <Link to="/checkout" style={{ textDecoration: "none" }}>
+          <div className="nav__itemShoppingCart">
+            <ShoppingCartIcon />
+            <span className="nav__itemLineTwo nav__shoppingCartCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
